4c7b241f4dbc240216105dbfde92c0d721623b9b
Jiri Kratochvil <kratochvil@klok.cz>
2011-08-26 00:17:02 +0200

    * refactoring severity/level etc. is now strictly called level * rename logger::filter() and logger::severity() to logger::level()

--- logxx.hpp
+++ logxx.hpp
@@ -77,7 +77,7 @@ typedef enum {
   info,
   debug,
   trace
-} severity;
+} level;
 
 
 /**
@@ -93,7 +93,7 @@ typedef enum {
  *
  *
  * filter_policy - decide what will happens with message
- * format_policy - add some envelope to message (e.g. time, severity, etc)
+ * format_policy - add some envelope to message (e.g. time, level, etc)
  * channel_policy - messages which goes through filter are sent into
  *
  * basic flow: 
@@ -127,7 +127,7 @@ public:
         boost::shared_ptr<self_t> root = loggers_[""];
         if(!root) root.reset(new self_t); // if not root loger in repository create new one with default settings
         r->channel_ = root->channel_;
-        r->filter_.severity_ = root->filter_.severity_;
+        r->filter_.level_ = root->filter_.level_;
       }
       loggers_[name] = r;
     }
@@ -140,15 +140,15 @@ public:
   /**
    * return instance of filter to send log on
    */
-  std::ostream& get(int severity) {
-    return filter_.filter(severity,channel(),format_);
+  std::ostream& get(int level) {
+    return filter_.filter(level,channel(),format_);
   }
 
   /**
    * shortcut of ::get() method
    */
-  std::ostream& operator()(int severity) {
-    return filter_.filter(severity,channel(),format_);
+  std::ostream& operator()(int level) {
+    return filter_.filter(level,channel(),format_);
   }
 
   virtual ~basic_logger() {
@@ -156,17 +156,17 @@ public:
   }
 
   /**
-   * setup filter level for given channel
+   * setup filter "at least" level for given channel
    *
    * usage:
-   * logxx::logger::log().filter(logxx::debug)
+   * logxx::logger::log().level(logxx::debug)
    */
-  inline void filter(int severity) { filter_.severity_ = severity; }
+  inline void level(int level) { filter_.level_ = level; }
 
   /**
-   * return current severity of filter
+   * return current level of filter
    */  
-  inline int severity() const { return filter_.severity_; }
+  inline int level() const { return filter_.level_; }
 
   /**
    * hex memory dump from @param addr and length @param len
@@ -174,7 +174,7 @@ public:
    * dump() is by default sent to debug level
    */
   void dump(void* addr, int len) {
-    if(filter_.severity_ < logxx::debug) return;
+    if(filter_.level_ < logxx::debug) return;
     unsigned char* a = reinterpret_cast<unsigned char*>(addr);
     get(logxx::debug) << boost::format("DUMP [%p(%d)]:") % addr % len << std::endl;
     for(int i = 0 ; i < len ; ++i ) {
@@ -191,8 +191,8 @@ protected:
   filter_policy filter_;
   static std::map<std::string, boost::shared_ptr<self_t> > loggers_;
 
-  basic_logger(int severity = default_level) {
-    filter_.severity_ = severity; 
+  basic_logger(int level = default_level) {
+    filter_.level_ = level; 
   }
 };
 
@@ -206,17 +206,17 @@ std::map<std::string, boost::shared_ptr<basic_logger<default_level, format_polic
  * message formater
  */
 struct basic_format {
-  virtual std::ostream& format(int severity, basic_channel& out) = 0;
+  virtual std::ostream& format(int level, basic_channel& out) = 0;
 };
 
 // IDEA: filter should receive ref to logger to allow identify name logger, 
 // instead of directly writing to channel just only receive message, create envelope and return it??
 struct std_format : basic_format {
-  std::ostream& format(int severity, basic_channel& out) {
+  std::ostream& format(int level, basic_channel& out) {
     return (out.stream()
         << boost::posix_time::second_clock::universal_time()
         << " [" << getpid() << "]"
-        << " <" << severity << ">"
+        << " <" << level << ">"
         << ": "
   );
 }
@@ -226,24 +226,24 @@ struct std_format : basic_format {
 /**
  * log filter - 
  *   it takes care on filtering sent messages
- *   std_filter will send everything with higher level than severity into null channel
+ *   std_filter will send everything with higher level than level into null channel
  *
  *   it is meant for extending eg. 
- *   - abort program when severity is lower than "critical"
+ *   - abort program when level is lower than "critical"
  *   - sent critical events to different channel eg. syslog
  *   - assertion for debug version
  *
  * some of ideas will be added into logxx lib in future
  */
 struct std_filter {
-  std::ostream& filter(int severity, basic_channel& s, basic_format& f) {
+  std::ostream& filter(int level, basic_channel& s, basic_format& f) {
     static nullstream null;
-    return severity <= severity_ 
-      ? f.format(severity,s)
+    return level <= level_ 
+      ? f.format(level,s)
       : null;
   }
 
-  int severity_;
+  int level_;
 };
 
 /**
@@ -256,13 +256,13 @@ typedef class basic_logger<LOGXX_DEFAULT_LEVEL , std_format, std_filter> logger;
 #define LOG_CHANNEL(ch) LOGXX_DEFAULT_LOGGER::log().channel(ch)
 #define LOG_CHANNEL_(sink, ch) LOGXX_DEFAULT_LOGGER::log(sink).channel(ch)
 
-#define LOG(level) if (LOGXX_DEFAULT_LOGGER::log().severity() >= logxx::level) LOGXX_DEFAULT_LOGGER::log().get(logxx::level)
-#define LOG_(sink, level) if (LOGXX_DEFAULT_LOGGER::log(sink).severity() >= logxx::level) LOGXX_DEFAULT_LOGGER::log(sink).get(logxx::level)
+#define LOG(lvl) if (LOGXX_DEFAULT_LOGGER::log().level() >= logxx::lvl) LOGXX_DEFAULT_LOGGER::log().get(logxx::lvl)
+#define LOG_(sink, lvl) if (LOGXX_DEFAULT_LOGGER::log(sink).level() >= logxx::lvl) LOGXX_DEFAULT_LOGGER::log(sink).get(logxx::lvl)
 
-#define DUMP(addr, len) if (LOGXX_DEFAULT_LOGGER::log().severity() >= logxx::debug) LOGXX_DEFAULT_LOGGER::log().dump((void*)addr,len)
+#define DUMP(addr, len) if (LOGXX_DEFAULT_LOGGER::log().level() >= logxx::debug) LOGXX_DEFAULT_LOGGER::log().dump((void*)addr,len)
 
 
-#define LOG_IF(condition, level) if ((condition) && LOGXX_DEFAULT_LOGGER::log().severity() >= logxx::level) LOGXX_DEFAULT_LOGGER::log().get(logxx::level)
-#define LOG_IF_(condition, sink, level) if ((condition) && LOGXX_DEFAULT_LOGGER::log(sink).severity() >= logxx::level) LOGXX_DEFAULT_LOGGER::log(sink).get(logxx::level)
+#define LOG_IF(condition, lvl) if ((condition) && LOGXX_DEFAULT_LOGGER::log().level() >= logxx::lvl) LOGXX_DEFAULT_LOGGER::log().get(logxx::lvl)
+#define LOG_IF_(condition, sink, lvl) if ((condition) && LOGXX_DEFAULT_LOGGER::log(sink).level() >= logxx::lvl) LOGXX_DEFAULT_LOGGER::log(sink).get(logxx::lvl)
 
 #endif /* end of include guard: LOGXX_5JKQSPQI */
